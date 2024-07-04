13f85369214fd3bb27f842ca8f6ede6981bbf75d
junyangchan <kardxsg@gmail.com>
2024-07-04 17:45:29 +0800

    feat: push drawer to the right side

--- web/components/ui/ui-layout.tsx
+++ web/components/ui/ui-layout.tsx
@@ -79,35 +79,11 @@ export function UiLayout({ children }: { children: ReactNode }) {
 
   return (
     <div className="flex flex-col h-screen">
-      <div className="drawer flex flex-1">
+      <div className="drawer drawer-end flex flex-1">
         <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
         <div className="drawer-content flex flex-col w-full">
           <div className="w-full navbar ">
-            <div className="flex-none md:hidden">
-              <label
-                htmlFor="my-drawer-3"
-                aria-label="open sidebar"
-                className="btn btn-square btn-ghost"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  className="inline-block w-6 h-6 stroke-current"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  ></path>
-                </svg>
-              </label>
-            </div>
-            <Link
-              className="hidden md:flex px-4 items-center gap-2 w-[480px]"
-              href="/"
-            >
+            <Link className="flex px-4 items-center gap-2 w-[480px]" href="/">
               <span className="text-2xl font-bold uppercase">HashFeed</span>
               <Image src={logo} alt={'logo'} width={30} height={30} />
             </Link>
@@ -125,7 +101,29 @@ export function UiLayout({ children }: { children: ReactNode }) {
                 ))}
               </ul>
             </div>
+
             <div className="navbar-end w-full gap-4">
+              <div className="flex-none md:hidden">
+                <label
+                  htmlFor="my-drawer-3"
+                  aria-label="open sidebar"
+                  className="btn btn-square btn-ghost"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    className="inline-block w-6 h-6 stroke-current"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    ></path>
+                  </svg>
+                </label>
+              </div>
               <div className="hidden">
                 <ThemeComponent toggleTheme={toggleTheme} theme={theme} />
               </div>
