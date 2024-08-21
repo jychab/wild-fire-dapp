f97ecafd958daa5ad21507cfb6efadb457eaa336
junyangchan <kardxsg@gmail.com>
2024-08-21 12:13:07 +0800

    feat: add open graph metada tags

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -16,7 +16,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <script src="https://terminal.jup.ag/main-v3.js" data-preload />
+        <title>HashFeed</title>
+        <meta property="og:title" content="Airdrop to share content" />
+        <meta property="og:type" content="blinks" />
+        <meta property="og:url" content="https://hashfeed.social" />
+        <meta
+          property="og:image"
+          content="https://buckets.hashfeed.social/CtiPMWDvrYm8SaWwuVJWWbq9qHCsGZjxVi4RShrgtyCY/media/9d441cd6-a8d0-499c-847d-5c399ef7c0ff"
+        />
       </head>
       <body>
         <ReactQueryProvider>
