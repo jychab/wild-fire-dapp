6c6223382709881708aea4297e639d9d7e8e6a38
junyangchan <kardxsg@gmail.com>
2024-09-01 02:33:12 +0800

    feat: add metadata for creation page

--- web/app/mint/create/page.tsx
+++ web/app/mint/create/page.tsx
@@ -1,4 +1,23 @@
 import { CreatePanel } from '@/components/create/create-feature';
+import { Metadata } from 'next';
+
+export async function generateMetadata({}): Promise<Metadata> {
+  return {
+    openGraph: {
+      title: 'Create your token',
+      description:
+        'Create blinks code-free and share them by airdropping tokens with our custom tool. Earn fees on every transfer.',
+      url: 'https://blinksfeed.com/mint/create',
+      images: [
+        {
+          url: 'https://buckets.blinksfeed.com/blinksfeed/main.gif',
+          alt: '',
+        },
+      ],
+      type: 'website',
+    },
+  };
+}
 
 export default function page() {
   return (
